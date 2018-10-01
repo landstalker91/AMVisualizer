e9927276eeb8a68409777b6f3a2ee4631bec9440
a.harchenkov <a.harchenkov@AHARCHENKOV1>
2018-10-01 17:53:34 +0300

    Create Graph Controller

--- AMVisualizer/Controllers/HomeController.cs
+++ AMVisualizer/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace AMVisualizer.Controllers
             return View();
         }
 
+        public IActionResult Graph()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
